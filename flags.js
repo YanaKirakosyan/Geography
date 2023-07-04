5681a7e352536737aba4af75e5ba4d85df7371bf
YanaKirakosyan <yana.kirakosyan@tumo.org>
2023-07-04 17:45:44 +0400

    04.07.23

---
+++ flags.js
@@ -0,0 +1,50 @@
+const imgs = [
+    {
+        "nkar": "img/Argentina.png",
+        "name": "Արգենտինա"
+    },
+    {
+        "nkar": "img/China.png",
+        "name": "Չինաստան"
+    },
+    {
+        "nkar": "img/Japan.png",
+        "name": "Ճապոնիա"
+    },
+    {
+        "nkar": "img/poland.png",
+        "name": "Լեհաստան"
+    },
+    {
+        "nkar": "img/SK.png",
+        "name": "Հարավային Կորեա"
+    },
+    {
+        "nkar": "img/Syunik.png",
+        "name": "Սյունիք"
+    },
+    {
+        "nkar": "img/Ararat.png",
+        "name": "Արարատ"
+    },
+    {
+        "nkar": "img/austria.png",
+        "name": "Ավստրիա"
+    },
+    {
+        "nkar": "img/Everest.jpg",
+        "name": "Էվերեստ"
+    },
+    {
+        "nkar": "img/Map.png",
+        "name": "Ռուսաստան"
+    },
+    {
+        "nkar": "img/morocco.jpg",
+        "name": "Մարոկկո"
+    },
+    {
+        "nkar": "img/SA.png",
+        "name": "Հարավային Աֆրիկա"
+    },
+]
--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 let seconds = 10;
+let correctAnswer = 0;
+let incorrectAnswer = 0;
+
 
 function getElement(id) {
   return document.getElementById(id);
 }
 
+
+function getRandomImgs() {
+  var randomItem = Math.floor(Math.random(imgs.length - 1) * 10)
+  return imgs[randomItem];
+}
+
+
+function flags() {
+  coun = getRandomImgs();
+  if (coun == undefined) {
+    coun = getRandomImgs();
+  }
+  getElement("nkar").src = coun.nkar;
+}
+
+
 function timer() {
   getElement("time").innerHTML = seconds;
   let countdown = setInterval(function () {
+    flags()
     seconds--;
     getElement("time").textContent = seconds;
     if (seconds <= 0) clearInterval(countdown);
@@ -15,4 +35,24 @@ function timer() {
 }
 
 
+function check() {
+  let input;
+  try {
+    input = document.querySelector('input[name = "option"]:checked').value;
+  } catch {
+    return;
+  }
+  correctAnswer++;
+  getElement("score").innerHTML = correctAnswer;
+  clearInterval(checkInterval);
+}
+
+
+function finish() {
+  clearInterval(checkInterval);
+  let percentage = 100;
+  getElement("alertaccuracy").innerHTML = `der ardyunqn e ${percentage}%`;
+}
+
+let checkInterval = setInterval(check, 50);
 timer();
